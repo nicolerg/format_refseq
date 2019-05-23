50bcb6566718adc4ee2459d6952f3196abd8acde
nicolerg <nicolerg@stanford.edu>
2019-05-23 16:22:50 -0700

    merge files after job array is done

--- make_db_from_refseq_workflow.sh
+++ make_db_from_refseq_workflow.sh
@@ -12,8 +12,12 @@ base=$1
 ## download code from GitHub 
 ################################################################################################################################ 
 
-srcdir=${base}/src
-mkdir ${srcdir}
+if [ ! -d "${base}/src/format_refseq" ]; then 
+	mkdir -p ${base}/src
+	cd ${base}/src
+	git clone http://github.com/nicolerg/format_refseq.git
+fi
+srcdir=${base}/src/format_refseq
 
 ################################################################################################################################  
 ## download RefSeq files 
@@ -25,19 +29,23 @@ for db in viral bacteria archaea fungi; do
 
 	# get a list of files on the FTP server 
 	cd ${base}/${db}
-	curl ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/ > tmp
-	cut -f 16 -d ' ' tmp | grep "genomic.fna" > file_list
+	curl --max-time 30 --retry 10 ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/ > tmp
+	grep "genomic.fna" tmp | sed "s/.* //" > file_list
 
 	# download FNA files 
 	while read f; do
-		wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/${f}
+		if [ ! -f "${base}/${db}/${f}" ]; then 
+			wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/${f}
+		fi
 	done < file_list
 
 	# download GBFF files (used to make headers)
 	cd ${base}/_${db}
-	cut -f 16 -d ' ' ${base}/${db}/tmp | grep "genomic.gbff" > file_list
+	grep "genomic.gbff" ${base}/${db}/tmp | sed "s/.* //" > file_list
 	while read f; do
-		wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/${f}
+		if [ ! -f "${base}/_${db}/${f}" ]; then 
+			wget ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/${f}
+		fi
 	done < file_list
 
 	rm ${base}/${db}/tmp
@@ -63,6 +71,16 @@ num_tasks1=$(cat ${base}/gbff_list | wc -l)
 
 qsub -N format_headers -t 1-$num_tasks1 -o ${base}/log/format_headers_$PBS_ARRAYID.log ${srcdir}/format_headers.sh
 
+# for db in viral bacteria archaea fungi; do
+# 	for gbff in `ls ${base}/_${db} | grep "genomic.gbff"`; do 
+# 		python2 ${srcdir}/format_headers.py ${base}/_${db}/${gbff} &
+# 	done 
+# done 
+# wait 
+
+# merge headers
+qsub -hold_jid format_headers -N merge_maps ${srcdir}/merge.sh
+
 ################################################################################################################################  
 ## use maps to convert headers in FNA files (qsub)
 ################################################################################################################################ 
@@ -77,4 +95,10 @@ done
 num_tasks2=$(cat ${base}/fna_list | wc -l)
 
 # this needs to wait until JOB1 has finished 
-qsub -hold_jid format_headers -N replace_headers -o ${base}/log/replace_headers_$PBS_ARRAYID.log -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
+qsub -hold_jid format_headers,merge_maps -N replace_headers -o ${base}/log/replace_headers_$PBS_ARRAYID.log -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
+
+# for db in viral bacteria archaea fungi; do
+# 	for fna in `ls ${base}/${db} | grep "genomic.fna"`; do 
+# 		python2 ${srcdir}/replace_fna_headers.py ${base}/${db}/${fna} ${base}/headers_map.tsv &
+# 	done 
+# done 
---
+++ merge_header_map.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
+#PBS -V
+
+echo 'format_headers job array is complete'
+# merge *headers_map.tsv files
+for dir in viral bacteria archaea fungi; do
+	cat ${base}/_${dir}/*headers_map.tsv >> ${base}/headers_map.tsv
+done
