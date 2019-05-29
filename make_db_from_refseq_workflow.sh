aaeb0a5ac21eec197f77321668769dfd2dbd6a3b
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 14:07:10 -0700

    Update make_db_from_refseq_workflow.sh

--- make_db_from_refseq_workflow.sh
+++ make_db_from_refseq_workflow.sh
@@ -76,7 +76,7 @@ echo $srcdir
 
 echo 'about to submit format_headers.sh'
 
-qsub -wd ${base} -v SRCDIR=${srcdir} -w e -N format_headers -t 1-$num_tasks1 ${srcdir}/format_headers.sh
+qsub -wd ${base} -V -t 1-$num_tasks1 ${srcdir}/format_headers.sh
 
 echo 'just past format_headers qsub'
 
@@ -88,7 +88,7 @@ echo 'just past format_headers qsub'
 # wait 
 
 # merge headers
-qsub -wd ${base} -hold_jid_ad format_headers -N merge_maps ${srcdir}/merge_header_map.sh
+qsub -wd ${base} -hold_jid_ad format_headers ${srcdir}/merge_header_map.sh
 
 # # merge *headers_map.tsv files
 # for dir in viral bacteria archaea fungi; do
@@ -115,4 +115,4 @@ done
 num_tasks2=$(cat ${base}/fna_list | wc -l)
 
 # this needs to wait until JOB1 has finished 
-qsub -wd ${base} -v SRCDIR=${srcdir} -hold_jid merge_maps -N replace_headers -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
+qsub -wd ${base} -hold_jid merge_header -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
