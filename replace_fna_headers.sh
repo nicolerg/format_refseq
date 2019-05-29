88417940a3c992a00500dce299266e6a71d01452
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 12:11:40 -0700

    load python module

--- replace_fna_headers.sh
+++ replace_fna_headers.sh
@@ -3,5 +3,7 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 
+module load python/2.7.13
+
 INFILE=$(awk "NR==$PBS_ARRAYID" ${BASE}/fna_list)
 python2 ${SRCDIR}/replace_fna_headers.py $INFILE ${BASE}/headers_map.tsv
