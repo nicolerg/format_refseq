c586963669ba859d61288afdd5db1f9e4aeb8440
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 11:15:10 -0700

    change PBS variable

--- format_headers.sh
+++ format_headers.sh
@@ -3,5 +3,5 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 
-INFILE=$(awk "NR==$SGE_TASK_ID" ${base}/gbff_list)
+INFILE=$(awk "NR==$PBS_ARRAYID" ${base}/gbff_list)
 python2 ${srcdir}/format_headers.py $INFILE
