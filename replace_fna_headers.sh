d93d2ffaa8ca38a12cbd296628c59c5583e141de
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 11:22:42 -0700

    export variables with -v

--- replace_fna_headers.sh
+++ replace_fna_headers.sh
@@ -3,5 +3,5 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 
-INFILE=$(awk "NR==$PBS_ARRAYID" ${base}/fna_list)
-python2 ${srcdir}/replace_fna_headers.py $INFILE ${base}/headers_map.tsv
+INFILE=$(awk "NR==$PBS_ARRAYID" ${BASE}/fna_list)
+python2 ${SRCDIR}/replace_fna_headers.py $INFILE ${BASE}/headers_map.tsv
