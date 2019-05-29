235118cec13f428381c71d3d63232875e886aa6e
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 12:11:14 -0700

    load python

--- format_headers.sh
+++ format_headers.sh
@@ -3,5 +3,7 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 
+module load python/2.7.13
+
 INFILE=$(awk "NR==$PBS_ARRAYID" ${BASE}/gbff_list)
 python2 ${SRCDIR}/format_headers.py $INFILE
