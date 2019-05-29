2b40e29fcf91d2fe52ce0ced084dc69351ec4834
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 12:47:56 -0700

    change wd

--- merge_header_map.sh
+++ merge_header_map.sh
@@ -2,9 +2,10 @@
 
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
+#PBS -o log/format_headers_$PBS_ARRAYID.log 
 
 echo 'format_headers job array is complete'
 # merge *headers_map.tsv files
 for dir in viral bacteria archaea fungi; do
-	cat ${BASE}/_${dir}/*headers_map.tsv >> ${BASE}/headers_map.tsv
+	cat _${dir}/*headers_map.tsv >> headers_map.tsv
 done
