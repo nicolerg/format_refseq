eb6397ef86810c9f0a7154623ce9f57d104be46c
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 14:05:31 -0700

    Update merge_header_map.sh

--- merge_header_map.sh
+++ merge_header_map.sh
@@ -3,6 +3,7 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 #PBS -o log/merge_header_map_$PBS_ARRAYID.log 
+#PBS -N merge_header
 
 echo 'format_headers job array is complete'
 # merge *headers_map.tsv files
