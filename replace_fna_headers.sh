10bfdd6d91ec44f42a8362f1ad361594540ee896
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 12:48:46 -0700

    add wd

--- replace_fna_headers.sh
+++ replace_fna_headers.sh
@@ -2,8 +2,9 @@
 
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
+#PBS -o log/replace_fna_headers_$PBS_ARRAYID.log 
 
 module load python/2.7.13
 
-INFILE=$(awk "NR==$PBS_ARRAYID" ${BASE}/fna_list)
-python2 ${SRCDIR}/replace_fna_headers.py $INFILE ${BASE}/headers_map.tsv
+INFILE=$(awk "NR==$PBS_ARRAYID" fna_list)
+python2 ${SRCDIR}/replace_fna_headers.py $INFILE headers_map.tsv
