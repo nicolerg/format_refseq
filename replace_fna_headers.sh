5d102dff8e5a128850ce45c6999dcbfc704c73c1
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 11:16:01 -0700

    fix PBS variable

--- replace_fna_headers.sh
+++ replace_fna_headers.sh
@@ -3,5 +3,5 @@
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
 
-INFILE=$(awk "NR==$SGE_TASK_ID" ${base}/fna_list)
+INFILE=$(awk "NR==$PBS_ARRAYID" ${base}/fna_list)
 python2 ${srcdir}/replace_fna_headers.py $INFILE ${base}/headers_map.tsv
