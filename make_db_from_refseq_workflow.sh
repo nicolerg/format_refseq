738a3acb60827ea1337976dfcd0d0c8fc7a9f96f
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-29 09:00:22 -0700

    apply qsub and fix git clone

--- make_db_from_refseq_workflow.sh
+++ make_db_from_refseq_workflow.sh
@@ -16,7 +16,7 @@ mkdir -p $base
 if [ ! -d "${base}/src/format_refseq" ]; then 
 	mkdir -p ${base}/src
 	cd ${base}/src
-	git clone http://github.com/nicolerg/format_refseq.git
+	git clone https://github.com/nicolerg/format_refseq.git
 fi
 srcdir=${base}/src/format_refseq
 
@@ -31,7 +31,7 @@ for db in viral bacteria archaea fungi; do
 	# get a list of files on the FTP server 
 	cd ${base}/${db}
 	curl --max-time 30 --retry 10 ftp://ftp.ncbi.nlm.nih.gov/refseq/release/${db}/ > tmp
-	grep "genomic.fna" tmp | sed "s/.* //" > file_list
+	grep "genomic.fna" tmp | sed "s/.* //" | head -2 > file_list
 
 	# download FNA files 
 	while read f; do
@@ -42,7 +42,7 @@ for db in viral bacteria archaea fungi; do
 
 	# download GBFF files (used to make headers)
 	cd ${base}/_${db}
-	grep "genomic.gbff" ${base}/${db}/tmp | sed "s/.* //" > file_list
+	grep "genomic.gbff" ${base}/${db}/tmp | sed "s/.* //" | head -2 > file_list
 
 	while read f; do
 		if [ ! -f "${base}/_${db}/${f}" ]; then 
@@ -62,28 +62,16 @@ done
 
 mkdir -p ${base}/log
 
-# # make list of files for jobarray
-# for db in viral bacteria archaea fungi; do
-# 	for gbff in `ls ${base}/_${db} | grep "genomic.gbff"`; do 
-# 		echo ${base}/_${db}/${gbff} >> ${base}/gbff_list
-# 	done 
-# done 
-
-# num_tasks1=$(cat ${base}/gbff_list | wc -l)
-
-# qsub -N format_headers -t 1-$num_tasks1 -o ${base}/log/format_headers_$PBS_ARRAYID.log ${srcdir}/format_headers.sh
-
+# make list of files for jobarray
 for db in viral bacteria archaea fungi; do
 	for gbff in `ls ${base}/_${db} | grep "genomic.gbff"`; do 
-		python2 ${srcdir}/format_headers.py ${base}/_${db}/${gbff} &
+		echo ${base}/_${db}/${gbff} >> ${base}/gbff_list
 	done 
 done 
-wait 
 
-# merge *headers_map.tsv files
-for dir in viral bacteria archaea fungi; do
-	cat ${base}/_${dir}/*headers_map.tsv >> ${base}/headers_map.tsv
-done
+num_tasks1=$(cat ${base}/gbff_list | wc -l)
+
+qsub -N format_headers -t 1-$num_tasks1 -o ${base}/log/format_headers_$PBS_ARRAYID.log ${srcdir}/format_headers.sh
 
 # for db in viral bacteria archaea fungi; do
 # 	for gbff in `ls ${base}/_${db} | grep "genomic.gbff"`; do 
@@ -93,27 +81,27 @@ done
 # wait 
 
 # merge headers
-qsub -hold_jid format_headers -N merge_maps ${srcdir}/merge.sh
+qsub -hold_jid format_headers -N merge_maps ${srcdir}/merge_header_map.sh
+
+# # merge *headers_map.tsv files
+# for dir in viral bacteria archaea fungi; do
+# 	cat ${base}/_${dir}/*headers_map.tsv >> ${base}/headers_map.tsv
+# done
 
 ################################################################################################################################  
 ## use maps to convert headers in FNA files (qsub)
 ################################################################################################################################ 
 
-# # make list of files for jobarray
 # for db in viral bacteria archaea fungi; do
 # 	for fna in `ls ${base}/${db} | grep "genomic.fna"`; do 
-# 		echo ${base}/${db}/${fna} >> ${base}/fna_list
+# 		python2 ${srcdir}/replace_fna_headers.py ${base}/${db}/${fna} ${base}/headers_map.tsv &
 # 	done 
 # done 
 
-# num_tasks2=$(cat ${base}/fna_list | wc -l)
-
-# # this needs to wait until JOB1 has finished 
-# qsub -hold_jid format_headers -N replace_headers -o ${base}/log/replace_headers_$PBS_ARRAYID.log -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
-
+# make list of files for jobarray
 for db in viral bacteria archaea fungi; do
 	for fna in `ls ${base}/${db} | grep "genomic.fna"`; do 
-		python2 ${srcdir}/replace_fna_headers.py ${base}/${db}/${fna} ${base}/headers_map.tsv &
+		echo ${base}/${db}/${fna} >> ${base}/fna_list
 	done 
 done 
 
@@ -121,9 +109,3 @@ num_tasks2=$(cat ${base}/fna_list | wc -l)
 
 # this needs to wait until JOB1 has finished 
 qsub -hold_jid format_headers,merge_maps -N replace_headers -o ${base}/log/replace_headers_$PBS_ARRAYID.log -t 1-$num_tasks2 ${srcdir}/replace_fna_headers.sh
-
-# for db in viral bacteria archaea fungi; do
-# 	for fna in `ls ${base}/${db} | grep "genomic.fna"`; do 
-# 		python2 ${srcdir}/replace_fna_headers.py ${base}/${db}/${fna} ${base}/headers_map.tsv &
-# 	done 
-# done 
