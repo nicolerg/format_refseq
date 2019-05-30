765d89eeda06542ebe5cd22a31d99f65160f70b9
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-30 09:01:08 -0700

    Update merge_header_map.sh

--- merge_header_map.sh
+++ merge_header_map.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
-#PBS -V
-#PBS -o log/merge_header_map_$PBS_ARRAYID.log 
-#PBS -N merge_header
 
 # merge *headers_map.tsv files
 for dir in viral bacteria archaea fungi; do
-	cat _${dir}/*headers_map.tsv >> headers_map.tsv
+	cat ${base}/_${dir}/*headers_map.tsv >> ${base}/headers_map.tsv
 done
