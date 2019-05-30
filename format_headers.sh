cc9cb05ec8ccfbe86459eddf66b24de2dbf76f65
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-05-30 10:10:48 -0700

    Update format_headers.sh

--- format_headers.sh
+++ format_headers.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
+#PBS -V
 
 module load python/2.7.13
 
-INFILE=$(awk "NR==$PBS_ARRAYID" ${base}/gbff_list)
-python2 ${srcdir}/format_headers.py $INFILE
+INFILE=$(awk "NR==$PBS_ARRAYID" gbff_list)
+python2 ${SRCDIR}/format_headers.py $INFILE
