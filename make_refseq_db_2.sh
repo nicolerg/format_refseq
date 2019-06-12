67274437d5c8ebcacd85b1f0369754ad7d00c51c
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-06-12 09:43:40 -0700

    Update make_refseq_db_2.sh

--- make_refseq_db_2.sh
+++ make_refseq_db_2.sh
@@ -12,9 +12,22 @@ mkdir -p $base
 # max number of tasks = 500
 
 mkdir -p ${base}/chunks
-split -n 480 --numeric-suffixes=1 ${base}/gbff_list ${base}/chunks/gbff_list_chunk_
+rm -f ${base}/chunks/*
+max_jobs=480
+files=$(cat ${base}/gbff_list | wc -l)
+num_lines_per_chunk=$((files / max_jobs))
 
-JOB1=`qsub -d ${base} -w ${base} -N format_headers -V -v SRCDIR=${srcdir},BASEDIR=${base} -t 1-480 -o log/format_headers_$PBS_ARRAYID-o.log -e log/format_headers_$PBS_ARRAYID-e.log ${srcdir}/format_headers_chunk.sh`
+if [[ $num_lines_per_chunk == "0" ]]; then 
+	echo ${base}/gbff_list > ${base}/chunk_list
+else 
+	split -l $num_lines_per_chunk ${base}/gbff_list ${base}/chunk/gbff_list_chunk_
+	ls ${base}/chunk > ${base}/chunk_list
+	sed -i "s:^:${base}/chunk_list/:" ${base}/chunk_list
+fi
+
+num_chunks=$(cat ${base}/chunk_list | wc -l)
+
+JOB1=`qsub -d ${base} -w ${base} -N format_headers -V -v SRCDIR=${srcdir},BASEDIR=${base} -t 1-${num_chunks} -o log/format_headers_$PBS_ARRAYID-o.log -e log/format_headers_$PBS_ARRAYID-e.log ${srcdir}/format_headers_chunk.sh`
 JOB1=`echo $JOB1 | sed "s/\..*//"`
 echo $JOB1
 
