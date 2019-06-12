a98fd5198f623df461b22b2e467563ca1fb37f35
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-06-12 13:02:40 -0700

    Update format_headers_chunk.sh

--- format_headers_chunk.sh
+++ format_headers_chunk.sh
@@ -2,6 +2,10 @@
 
 #PBS -l walltime=10:00:00,nodes=1:ppn=1,mem=1gb
 #PBS -V
+#PBS -o log/format_headers_$PBS_ARRAYID-o.log 
+#PBS -e log/format_headers_$PBS_ARRAYID-e.log
+
+echo 'PBS_ARRAYID: $PBS_ARRAYID'
 
 module load python/2.7.13
 
