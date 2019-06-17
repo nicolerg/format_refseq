b561fa0a5c36d6153c98f5319a8944f064b919af
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-06-17 13:48:05 -0700

    Create make_db_from_refseq_qsub.sh

---
+++ make_db_from_refseq_qsub.sh
@@ -0,0 +1,7 @@
+#PBS -d .
+#PBS -l walltime=72:00:00
+#PBS -l nodes=1:ppn=16
+#PBS -o log/make_db_from_refseq-o.log
+#PBS -e log/make_db_from_refseq-e.log
+
+/bin/bash src/format_refseq/make_db_from_refseq.sh $(pwd)
