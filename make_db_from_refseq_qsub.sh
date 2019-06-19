a8f7f26b25089209beb2c1b2ce7cf804ffc75fd8
nicolerg <32148717+nicolerg@users.noreply.github.com>
2019-06-19 09:15:42 -0700

    Update make_db_from_refseq_qsub.sh

--- make_db_from_refseq_qsub.sh
+++ make_db_from_refseq_qsub.sh
@@ -1,6 +1,6 @@
 #PBS -l walltime=72:00:00
-#PBS -l nodes=2:ppn=10
-#PBS -l mem=5G
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=10G
 #PBS -o log/make_db_from_refseq-o.log
 #PBS -e log/make_db_from_refseq-e.log
 
